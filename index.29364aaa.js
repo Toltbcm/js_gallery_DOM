edebfc57d59a9b556a00f7d1a53ae7fb8ff35bb0
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-07-20 03:22:08 +0300

    make build

---
+++ index.29364aaa.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){var r;t.preventDefault(),e.src=null===(r=t.target.closest(".list-item__link"))||void 0===r?void 0:r.href});
+//# sourceMappingURL=index.29364aaa.js.map
